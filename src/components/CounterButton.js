1ecf73d971f20513e711c7714f954ad012cd5c81
Eu <eulier1@gmail.com>
2020-11-12 16:46:16 -0300

    fix test

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class CounterButton extends Component {
   constructor(props) {
     super(props);
-    this.state = {count: 5};
+    this.state = {count: 1};
   }
 
   shouldComponentUpdate(nextProps, nextState) {
