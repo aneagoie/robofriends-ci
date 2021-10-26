67458c69b6c38420fc431f2005d8d3e754204c01
Andrei Neagoie <aneagoie@MacBook-Pro-2.lan>
2021-10-26 10:45:40 -0400

    updates

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,22 +1,23 @@
-import React from "react";
-import Card from "./Card";
+import React from 'react';
+import Card from './Card';
 
 const CardList = ({ robots }) => {
-  console.log("CardList");
   return (
     <div>
-      {robots.map((user, i) => {
-        return (
-          <Card
-            key={i}
-            id={robots[i].id}
-            name={robots[i].name}
-            email={robots[i].email}
-          />
-        );
-      })}
+      {
+        robots.map((user, i) => {
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+              />
+          );
+        })
+      }
     </div>
   );
-};
+}
 
 export default CardList;
